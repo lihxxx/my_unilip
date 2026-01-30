627a16b1955fce53565a1385a859127dc09380e0
lihxxx <lihxxxxxx@gmail.com>
2026-01-30 16:15:39 +0800

    update

--- scripts/run_unilip_1b_unified_stage3.sh
+++ scripts/run_unilip_1b_unified_stage3.sh
@@ -16,7 +16,7 @@ export EDIT_IMG_FOLDER="${BASE_DIR}/data/edit_sft"
 unset WANDB_DISABLED
 export WANDB_API_KEY="3ed65eb52edcc37a5e278a82dd874b44d4ffadb7"
 export WANDB_PROJECT="unilip_umm"
-export WANDB_NAME="unilip_intern_vl_1b_sft_self_repa"
+export WANDB_NAME="unilip_intern_vl_1b_sft_self_repa_w05xd6"
 export OUTPUT_FOLDER="${BASE_DIR}/results/${WANDB_NAME}"
 
 # ============== REPA配置 ==============
