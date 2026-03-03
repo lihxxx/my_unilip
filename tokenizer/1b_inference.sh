a6733f6daab51de5cc0112fb94cc271bb401526a
lhxxxmad <lhx7652@gmail.com>
2026-03-03 15:03:48 +0800

    update

--- tokenizer/1b_inference.sh
+++ tokenizer/1b_inference.sh
@@ -1,6 +1,6 @@
 WANDB_MODE=offline accelerate launch --num_machines=1 --num_processes=1 --machine_rank=0 --main_process_ip=127.0.0.1 --main_process_port=9997 --same_network \
-    scripts/inference.py config=/mnt/tidal-alsh01/dataset/zeus/lihongxiang/unified_model/my_unilip/tokenizer/checkpoints/unilip_1b_dual_stream_pixel_distill_224_IN_GAN25k/config.yaml \
-    checkpoint_path=/mnt/tidal-alsh01/dataset/zeus/lihongxiang/unified_model/my_unilip/tokenizer/checkpoints/unilip_1b_dual_stream_pixel_distill_224_IN_GAN25k/checkpoint-50000/ema_model/pytorch_model.bin \
+    scripts/inference.py config=/mnt/tidal-alsh01/dataset/zeus/lihongxiang/unified_model/my_unilip/tokenizer/checkpoints/64GCF_bs4stage2F64stage2_1b_dualstreamPixTrans616_10x00_448E256_pretrain_GAN00k_lr3e4x01E1e5W10k_100k/config.yaml \
+    checkpoint_path=/mnt/tidal-alsh01/dataset/zeus/lihongxiang/unified_model/my_unilip/tokenizer/checkpoints/64GCF_bs4stage2F64stage2_1b_dualstreamPixTrans616_10x00_448E256_pretrain_GAN00k_lr3e4x01E1e5W10k_100k/checkpoint-100000/ema_model/pytorch_model.bin \
     model.use_padding=False \
-    img_path="xxx.jpg" \
-    dataset.preprocessing.resize_shorter_edge=224 dataset.preprocessing.crop_size=224
+    img_path="/mnt/tidal-alsh01/dataset/zeus/lihongxiang/unified_model/my_unilip/demo/edit_input.jpg" \
+    dataset.preprocessing.resize_shorter_edge=448 dataset.preprocessing.crop_size=448
